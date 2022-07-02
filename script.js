773dc109d079f17cc132c35e8662c50da35942e6
IdrisGuloyev <idrisguloyev@gmail.com>
2022-07-02 14:33:26 +0600

    Имплементация Закрыть счет

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const inputLoginPin = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
+const inputCloseNickname = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 ////////////////////////   FOR EACH
@@ -145,6 +145,7 @@ const updateUi = function (account) {
   displayTotal(account);
 };
 
+// Event handlers
 let currentAccount;
 
 btnLogin.addEventListener('click', function (e) {
@@ -190,4 +191,23 @@ btnTransfer.addEventListener('click', function (e) {
     recipientAccount.transactions.push(TransferAmount);
     updateUi(currentAccount);
   }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseNickname.value === currentAccount.nickname &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const currentAccountIndex = accounts.findIndex(
+      account => account.nickname === currentAccount.nickname
+    );
+
+    accounts.splice(currentAccountIndex, 1);
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = 'Войдите в свой аккаунт';
+  }
+
+  inputCloseNickname.value = '';
+  inputClosePin.value = '';
 });
